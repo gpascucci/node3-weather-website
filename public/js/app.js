19f9f42bdee1cbe9afc7203ef8ff19178d3fa3af
Pascucci <greg.pascucci@cgi.com>
2020-08-09 16:27:07 -0700

    Initial Commit of Weather App

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Searching...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+        response.json().then(({ error, forecast, location }) => {
+            if (error) {
+                return messageOne.textContent =error
+            }
+    
+            messageOne.textContent = location
+            messageTwo.textContent = forecast
+    
+        })
+    })
+
+    console.log(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=810efd5f37c12b7c0a0b5a518a323c9a&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Weather service unavailable.')
+        } else if (body.error) {
+            callback('Unable to find weather for provided location.')
+        } else {
+            const {temperature, feelslike, weather_descriptions} = body.current
+            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out, but feels like ' + feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
